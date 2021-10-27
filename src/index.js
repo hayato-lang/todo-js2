69d46413f73a703891a55e48e733fe578a5da311
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-10-28 08:43:48 +0900

    タスクの追加機能(テキスト)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  const p = document.createElement("p");
+  p.className = "todo-title";
+  p.innerText = inputText;
+
+  const div = document.createElement("div");
+  div.className = "list-show";
+
+  li.appendChild(p);
+
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
